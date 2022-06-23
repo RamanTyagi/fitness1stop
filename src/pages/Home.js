c6fa0693fa9f48036f27442f9baaf2e701545b43
Raman Tyagi <ramantyag9873@gmail.com>
2022-06-23 20:16:38 +0530

    FetchedExercises

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,21 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
+
 import Exercises from '../components/Exercises';
-import SearchExercise from '../components/SearchExercise';
+import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
+
 const Home = () => {
-  return(
-      <Box>
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState('all');
+
+  return (
+    <Box>
       <HeroBanner />
-      <SearchExercise />
-      <Exercises />
-      </Box>
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+    </Box>
   );
 };
+
 export default Home;
